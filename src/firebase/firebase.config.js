3a1849f9191552fe47d42160eb9b499949394386
Shinigami0509 <walidbin.kamal64@gmail.com>
2025-06-18 12:07:19 +0600

    login & registration done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDAH0KDgz0gZBFfsDfENwDxR_1RDh441uM",
+    authDomain: "dragon-news-7a560.firebaseapp.com",
+    projectId: "dragon-news-7a560",
+    storageBucket: "dragon-news-7a560.firebasestorage.app",
+    messagingSenderId: "299606701320",
+    appId: "1:299606701320:web:9e0e380f08d2f59b08e8bd"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/pages/Register.jsx
@@ -0,0 +1,81 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../provider/AuthProvider';
+
+const Register = () => {
+
+    const { createNewUser, setUser } = useContext(AuthContext)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        // const name = e.target.name.value
+        // const photo = e.target.photo.value
+        // const email = e.target.email.value
+        // const password = e.target.password.value
+        const form = new FormData(e.target)
+        const name = form.get('name')
+        const photo = form.get('photo')
+        const email = form.get('email')
+        const password = form.get('password')
+        // console.log({name,photo,email,password})
+
+        createNewUser(email, password)
+            .then(res => {
+                const user = res.user
+                // console.log(user)
+                setUser(user)
+            })
+            .catch(error => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode, errorMessage)
+            })
+
+    }
+
+    return (
+        <div>
+            <div className='min-h-screen flex justify-center items-center'>
+                <div className="card bg-base-100 w-full max-w-lg shrink-0 rounded-none p-10">
+                    <h2 className='text-2xl font-semibold text-center mt-5'>Register your account</h2>
+                    <form onSubmit={handleSubmit} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Your Name</span>
+                            </label>
+                            <input name='name' type="text" placeholder="Enter your name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input name='photo' type="text" placeholder="Enter Photo URL" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input name='email' type="email" placeholder="Enter Email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input name='password' type="password" placeholder="Enter Password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-neutral rounded-none">Register</button>
+                        </div>
+                    </form>
+                    <p className="text-center font-semibold">
+                        Already Have an Account? <Link className='text-red-400' to='/auth/login'>Login</Link></p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
